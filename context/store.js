7632f9e6efdeacae6bb59a6cce6768c13bc00818
Rare Collectables <rarecollectablessales@gmail.com>
2025-06-20 13:48:46 +0100

    GA4 to stop tracking localhost event

--- context/store.js
+++ context/store.js
@@ -209,6 +209,8 @@ export function StoreProvider({ children }) {
             console.error('[store.js] Error parsing stored data:', parseErr, data);
             parsedData = {};
           }
+          // DEBUG: Log hydrated wishlist
+          console.log('[store.js] Hydrating wishlist:', parsedData.wishlist);
           dispatch({ 
             type: 'HYDRATE', 
             payload: {
@@ -231,6 +233,8 @@ export function StoreProvider({ children }) {
           cart: ensureArray(state.cart),
           wishlist: ensureArray(state.wishlist)
         }));
+        // DEBUG: Log persisted wishlist
+        console.log('[store.js] Persisted wishlist:', state.wishlist);
       } catch (error) {
         console.error('Error saving store:', error);
       }
